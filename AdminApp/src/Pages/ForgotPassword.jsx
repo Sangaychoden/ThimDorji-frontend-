ed1a919ea5342474251afce6aaa871befb2dd6cf
Sangaychoden <12220059.gcit2rub.edu.bt>
2025-11-21 03:10:17 +0600

    initial commit

--- AdminApp/src/Pages/ForgotPassword.jsx
+++ AdminApp/src/Pages/ForgotPassword.jsx
@@ -13,7 +13,7 @@ const ForgotPassword = () => {
   const [errors, setErrors] = useState({});
   const inputRefs = useRef([]);
 
-  const API_URL = import.meta.env.VITE_API_URL;
+  const API_URL = import.meta.env.VITE_API_URL || "https://thimdorji-backend-5.onrender.com";
 
   // 🔹 Styles (same)
   const pageStyle = {
