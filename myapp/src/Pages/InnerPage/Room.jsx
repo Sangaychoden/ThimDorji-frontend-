290c82cc434c7fb25902b70698d86b638b8525d5
anushia56 <12220026.gcit@rub.edu.bt>
2025-11-24 04:25:18 +0530

    update

--- myapp/src/Pages/InnerPage/Room.jsx
+++ myapp/src/Pages/InnerPage/Room.jsx
@@ -346,7 +346,7 @@ const Room = () => {
             <div className="flex items-center justify-center space-x-2">
 <hr className="w-[150px] h-[1px] text-[#8C8C8C]" />            </div>
             <h1 className="text-[22px] sm:text-2xl md:text-3xl 2xl:text-[38px] leading-7 sm:leading-8 md:leading-9 lg:leading-[42px] 2xl:leading-[52px] text-black mt-[30px] mb-[24px] font-inter font-semibold uppercase">
-              Rooms & Suites
+              Rooms
             </h1>
           </div>
 
