d508cf3ae7ce10333a6ff21da87ea2d4466506eb
Sangaychoden <12220059.gcit2rub.edu.bt>
2025-11-25 03:25:55 +0600

    fixed issues on crete booking in receptionist

--- AdminApp/vite.config.js
+++ AdminApp/vite.config.js
@@ -1,8 +1,23 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//     base: '/', 
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-    base: '/', 
+  base: '/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      }
+    }
+  }
 })
